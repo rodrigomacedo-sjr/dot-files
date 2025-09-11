d4e8725848362c94e9f377433032b388d89f8c58
rodrigomacedo-sjr <rodrigomacedo.sjr@gmail.com>
2025-09-11 15:07:11 -0300

    feat: update nvim config for transparency

---
+++ nvim/lua/plugins/transparency.lua
@@ -0,0 +1,13 @@
+return {
+  "xiyaowong/transparent.nvim",
+  opts = {
+    extra_groups = {
+      "NormalNC",
+      "NvimTreeNormal",
+      "NvimTreeNormalNC",
+      "BufferLineInactive",
+      "StatusLineNC",
+      "NeoTreeNormal"
+    },
+  },
+}
