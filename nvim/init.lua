78424b5c0baebc0edb91b5752ff4451c60c4afb6
rodrigomacedo-sjr <rodrigomacedo.sjr@gmail.com>
2025-11-01 23:12:26 -0300

    tchau

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,31 +1,19 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath,
   })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("vim-options")
 require("lazy").setup("plugins")
+
 vim.cmd.colorscheme("catppuccin-mocha")
+
 require("keymaps")
 require("commands")
 
-require("go")
-local format_sync_grp = vim.api.nvim_create_augroup("goimports", {})
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*.go",
-  callback = function()
-   require('go.format').goimports()
-  end,
-  group = format_sync_grp,
-})
-
-
 vim.opt.ttimeoutlen = 10
+
